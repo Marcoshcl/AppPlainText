446a94893a6865683e061e40debeb9dfc1808acb
Marcoshcl <marcoshenrique333@gmail.com>
2023-04-24 21:04:11 -0400

    Devtitans - app ongoing
    Signed-off-by: Marcoshcl <marcoshenrique333@gmail.com>

---
+++ TestApp/app/src/main/java/com/example/testapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.testapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button cadastrarUsuario;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        cadastrarUsuario = findViewById(R.id.loginButton);
+        cadastrarUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MainCadastro.class));
+            }
+        });
+    }
+}
